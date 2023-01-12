bb6da91059d9a9618f403c358eca67b67996f323
Davit Shkubuliani <d.shkubula@gmail.com>
2023-01-12 20:57:28 +0400

    Add margins beetwen components

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -9,7 +9,7 @@ const Portfolio = () => {
   //setProject(data);
 
   return (
-    <div name='portfolio' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+    <div name='portfolio' className='w-full md:h-screen text-gray-300 bg-[#0a192f] mb-32'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,7 +14,7 @@ import MySQL from '../assets/mysql.png';
 
 const Skills = () => {
     return (
-        <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+        <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300 mb-56'>
             {/* Container */}
             <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
                 <div>
