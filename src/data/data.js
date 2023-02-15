226e83671c66e0511e9dad8486e3aced3bc8d738
Davit Shkubuliani <d.shkubula@gmail.com>
2023-02-16 00:05:39 +0400

    Update data.js file

--- src/data/data.js
+++ src/data/data.js
@@ -4,6 +4,7 @@ import ScnwProject from "../assets/projects/scandiweb-project.PNG";
 import GeorgianKitchen from "../assets/projects/georgian-kitchen.PNG";
 import EduSpaceChildren from "../assets/projects/educational-space-children.PNG";
 import TodoApp from "../assets/projects/todo-app.PNG";
+import RedBerryTask from "../assets/projects/redberry-task.PNG";
 
 export const data = [
   {
@@ -15,31 +16,31 @@ export const data = [
   },
   {
     id: 2,
+    name: "Redberry Task-Resume Builder(avalaible only 1920x1080)",
+    image: RedBerryTask,
+    github: "https://github.com/shkubu18/RedBerry-Task",
+    live: "https://redberry-task-davit-shkubuliani.vercel.app/",
+  },
+  {
+    id: 3,
     name: "Todo App with React(typescript)",
     image: TodoApp,
     github: "https://github.com/shkubu18/todo-app-react",
     live: "https://todoapp-davit-shkubuliani.vercel.app/",
   },
   {
-    id: 3,
-    name: "Weather App with React",
-    image: WeatherApp,
-    github: "https://github.com/shkubu18/Weather-App",
-    live: "https://weather-app-davit-shkubuliani.vercel.app/",
-  },
-  {
     id: 4,
-    name: "PHP(with OOP Approach), MySQL Project",
+    name: "Scandiweb Task-PHP(with OOP Approach), MySQL",
     image: ScnwProject,
     github: "https://github.com/shkubu18/scandiweb-test-assignment",
     live: "https://davit-shkubuliani.000webhostapp.com/",
   },
   {
     id: 5,
-    name: "Georgian Kitchen with React",
-    image: GeorgianKitchen,
-    github: "https://github.com/shkubu18/react-georgian-kitchen",
-    live: "https://georgian-kitchen.vercel.app/",
+    name: "Weather App with React",
+    image: WeatherApp,
+    github: "https://github.com/shkubu18/Weather-App",
+    live: "https://weather-app-davit-shkubuliani.vercel.app/",
   },
   {
     id: 6,
