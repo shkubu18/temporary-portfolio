2b715d8fc30d8fe4910f584b16b3377a90c881ed
Davit Shkubuliani <d.shkubula@gmail.com>
2023-02-15 23:51:19 +0400

    Update data.js file

--- src/data/data.js
+++ src/data/data.js
@@ -1,53 +1,51 @@
-import WeatherApp from '../assets/projects/weather-app.PNG';
-import ECommerce from '../assets/projects/e-commerce.PNG';
-import ScnwProject from '../assets/projects/scandiweb-project.PNG';
-import GeorgianKitchen from '../assets/projects/georgian-kitchen.PNG';
-import EduSpaceChildren from '../assets/projects/educational-space-children.PNG';
-import TodoApp from '../assets/projects/todo-app.PNG';
+import WeatherApp from "../assets/projects/weather-app.PNG";
+import ECommerce from "../assets/projects/e-commerce.PNG";
+import ScnwProject from "../assets/projects/scandiweb-project.PNG";
+import GeorgianKitchen from "../assets/projects/georgian-kitchen.PNG";
+import EduSpaceChildren from "../assets/projects/educational-space-children.PNG";
+import TodoApp from "../assets/projects/todo-app.PNG";
 
 export const data = [
-    {
-        id: 1,
-        name: "Weather App with React",
-        image: WeatherApp,
-        github: "https://github.com/shkubu18/Weather-App",
-        live: "https://weather-app-davit-shkubuliani.vercel.app/",
-    },
-    {
-        id: 2,
-        name: "Todo App with React(typescript)",
-        image: TodoApp,
-        github: "https://github.com/shkubu18/todo-app-react",
-        live: "https://todoapp-davit-shkubuliani.vercel.app/",
-    },
-    {
-        id: 3,
-        name: "E-Commerce-Demo",
-        image: ECommerce,
-        github: "https://github.com/shkubu18/E-commerce-demo",
-        live: "https://shkubu18.github.io/E-commerce-demo/",
-    },
-    {
-        id: 4,
-        name: "PHP(with OOP Approach), MySQL Project",
-        image: ScnwProject,
-        github: "https://github.com/shkubu18/scandiweb-test-assignment",
-        live: "https://davit-shkubuliani.000webhostapp.com/",
-    },
-    {
-        id: 5,
-        name: "Georgian Kitchen with React",
-        image: GeorgianKitchen,
-        github: "https://github.com/shkubu18/react-georgian-kitchen",
-        live: "https://georgian-kitchen.vercel.app/",
-    },
-    {
-        id: 6,
-        name: "Educational Space For Children",
-        image: EduSpaceChildren,
-        github: "https://github.com/shkubu18/Educational-Space-For-Children",
-        live: "https://shkubu18.github.io/Educational-Space-For-Children/",
-    }
-
-
-]
+  {
+    id: 1,
+    name: "Audiophile Shop with React",
+    image: ECommerce,
+    github: "https://github.com/shkubu18/audiophile-shop-react",
+    live: "https://audiophileshop-davit-shkubuliani.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "Todo App with React(typescript)",
+    image: TodoApp,
+    github: "https://github.com/shkubu18/todo-app-react",
+    live: "https://todoapp-davit-shkubuliani.vercel.app/",
+  },
+  {
+    id: 3,
+    name: "Weather App with React",
+    image: WeatherApp,
+    github: "https://github.com/shkubu18/Weather-App",
+    live: "https://weather-app-davit-shkubuliani.vercel.app/",
+  },
+  {
+    id: 4,
+    name: "PHP(with OOP Approach), MySQL Project",
+    image: ScnwProject,
+    github: "https://github.com/shkubu18/scandiweb-test-assignment",
+    live: "https://davit-shkubuliani.000webhostapp.com/",
+  },
+  {
+    id: 5,
+    name: "Georgian Kitchen with React",
+    image: GeorgianKitchen,
+    github: "https://github.com/shkubu18/react-georgian-kitchen",
+    live: "https://georgian-kitchen.vercel.app/",
+  },
+  {
+    id: 6,
+    name: "Educational Space For Children",
+    image: EduSpaceChildren,
+    github: "https://github.com/shkubu18/Educational-Space-For-Children",
+    live: "https://shkubu18.github.io/Educational-Space-For-Children/",
+  },
+];
